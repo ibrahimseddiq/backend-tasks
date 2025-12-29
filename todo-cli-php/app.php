ecd901ee1deb976eda2eae06801c2dafa9a7ad75
Ibrahim Seddiq <ibrahimgheit84@gmail.com>
2025-12-29 11:23:51 +0200

    Fixing Some Bugs and Maintanence The Deleting and Status Function

--- todo-cli-php/app.php
+++ todo-cli-php/app.php
@@ -8,7 +8,7 @@ if (!file_exists($json_file)) {
 echo"\n== To-Do List Program ==\n\n";
 
 $json_content = file_get_contents($json_file);
-$tasks = json_decode($json_content, associative: true); // tasks is an array 
+$tasks = json_decode($json_content, true); // tasks is an array 
 
 $counter = count($tasks);
 
@@ -23,8 +23,11 @@ while(true)
     echo "Enter a Choise: ";
     $number = trim(fgets(STDIN));
     echo"\n---------------------\n\n";
-    if($number == "1") //Adding new task
+    if($number === "1") //Adding new task
     {
+        
+        $json_content = file_get_contents($json_file);
+        $tasks = json_decode($json_content, associative: true); // tasks is an array 
         $counter++;
         echo "Task Name: ";
         $task_name = trim(fgets(STDIN));
@@ -34,48 +37,73 @@ while(true)
         echo "Task added Successfuly!\n";
     }
 
-    elseif($number == "2") // Status
+        elseif($number === "2") // Status
     {
-
-        echo " Task Number for Make it Done: ";
+        $json_content = file_get_contents($json_file);
+        $tasks = json_decode($json_content, associative: true); // tasks is an array 
+        $json_content = file_get_contents($json_file); $tasks = json_decode($json_content, associative: true); // tasks is an array 
+        echo " Task Number for Make it Done: "; 
         $task_number = trim(fgets(STDIN));
-        foreach($tasks as &$task)
+        $found = false;
+        foreach($tasks as &$task) 
         {
-            if($task["Task Number"] == $task_number)
+            if($task["Task Number"] == $task_number) 
             {
                 $task["Status"] = true;
+                $found = true;
+                break;
             }
         }
-        file_put_contents($json_file, json_encode($tasks));
-        echo "Task is Done!\n";
-
-    }
-
-    elseif($number == "3")
-    {
-        // Code of Printig the list
-        foreach ($tasks as $task)
+        if($found) 
         {
-            $state = "";
-            if($task["Status"] == true)
-                $state = "Done";
-            else
-                $state = "Not Done";
-
-            echo "Task({$task['Task Number']}): {$task['name']} => is $state!\n";
+            file_put_contents($json_file, json_encode($tasks));
+            echo "Task is Done!\n";
+        } 
+        else 
+        {
+            echo "Invalid Task Number!\n";
         }
-
     }
 
-    elseif($number == "4")
+
+    elseif($number === "3") 
     {
+        // Code of Printig the list 
+        $json_content = file_get_contents($json_file); 
+        $tasks = json_decode($json_content, associative: true); // tasks is an array 
+        foreach ($tasks as $task) 
+        { 
+            $state = ""; 
+            if($task["Status"] != true) 
+                $state = "Not Done"; 
+            else
+                $state = "Done"; 
+            echo "Task({$task['Task Number']}): {$task['name']} => is $state!\n"; 
+        } 
+    }
+    elseif($number === "4")
+    {
+        
+        $json_content = file_get_contents($json_file);
+        $tasks = json_decode($json_content, associative: true); // tasks is an array 
         echo "Task's Number To Delete: ";
-        $task_number = trim(fgets(STDIN)); 
-        Delete_Task($task_number, $tasks);
-        echo "The Task Deleted Successfully!\n";
+        $task_number = trim(fgets(STDIN));
+        $found = false;
+        Delete_Task($task_number, $tasks,$found);
+        if($found)
+        {
+            $counter--;
+            foreach($tasks as $i => &$task)
+            {
+                $task["Task Number"] = $i+1;            
+            }
+            
+            file_put_contents($json_file, json_encode($tasks, JSON_PRETTY_PRINT));
+        }
+        //file_put_contents($json_file, json_encode($tasks, JSON_PRETTY_PRINT));
     }
 
-    elseif($number == "5")
+    elseif($number === "5")
     {
         echo "Ok! See You Later!\n";
         break;
@@ -86,46 +114,25 @@ while(true)
         echo "Invalide Choise!\n";
     }
 }
-function Delete_Task($task_id,array &$Tasks) 
+function Delete_Task($task_id,array &$Tasks,bool &$found) 
 {
     $found = false;
     foreach($Tasks as $task)
     {
-        if($task["Task Number"] = $task_id)
+        if($task["Task Number"] == $task_id)
         {
             $found = true;
             break;
         }
     }
-    $Tasks = array_filter($Tasks,fn($value) => $value["Task Number"] != $task_id);
-    $Tasks = array_values($Tasks); // To Reset the Keys
+    if(!$found)
+        echo"Invalid ID!";
+    else
+    {
+        $Tasks = array_filter($Tasks,fn($value) => $value["Task Number"] != $task_id);
+        $Tasks = array_values($Tasks); // To Reset the Keys
+        echo"The Task Deleted Successfully!\n";
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
